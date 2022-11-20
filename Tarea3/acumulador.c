d7fa401b7aef96da4dcf9cb41e8131c7b7234fa7
NicolasCanales <nicolas.canales@sansano.usm.cl>
2022-11-20 03:54:58 -0300

    Cambio a threads y manejo de clientes

--- Tarea3/acumulador.c
+++ Tarea3/acumulador.c
@@ -1,132 +1,180 @@
-/*
-NAME:       TCPServer [Port number]
-SYNOPSIS:    TCPServer
-DESCRIPTION:  The program creates a TCP socket in the inet,
-              listens for connections from TCPClients, accepts
-              clients into private sockets, and forks
-              an echo process to ``serve'' the cleint.
-*/
-
 #include <stdio.h>
-#include <errno.h>
-#include <sys/time.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <pthread.h>
+#include <semaphore.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <signal.h>
 
 #include <sys/param.h>
-#include <sys/socket.h>
 #include <sys/file.h>
+#include <sys/socket.h>
+#include <sys/time.h>
+#include <sys/types.h>
 
 #include <netinet/in_systm.h>
 #include <netinet/in.h>
 #include <netinet/ip.h>
 #include <netinet/ip_icmp.h>
 #include <netdb.h>
-#include <arpa/inet.h>
-#include <strings.h>
-#include <stdlib.h>
 
-#include <unistd.h> /*gethostname*/
+#include <errno.h>
 
+#define MAX_THREADS 20
+#define MAX_BUFF_SIZE 256
+
+sem_t x, y;
+pthread_t tid;
+pthread_t teller_threads[MAX_THREADS];
+pthread_t visualizer_threads[MAX_THREADS];
+int n_tellers = 0;
+int n_visualizers = 0;
 
 int A = 0;
 int B = 0;
 
+int A_last = 0;
+int B_last = 0;
+int max(int x, int y);
+void recieveVote(void* param);
+void refreshData(void* param);
 
-#define MAXHOSTNAME 80
-void reusePort(int sock);
-void EchoServe(int psd, struct sockaddr_in from);
 
 int main(int argc, char *argv[])
 {
-    int sd, psd;
-    struct sockaddr_in server;
-    struct hostent *hp, *gethostbyname();
-    struct servent *sp;
-    struct sockaddr_in from;
-    int fromlen;
-    int length;
-    char ThisHost[MAXHOSTNAME];
+  fd_set rset;
+  // Initialize variables
+  int serverSocket_t, newSocket_t;
+  struct sockaddr_in serverAddr_t;
+  struct sockaddr_storage serverStorage_t;
 
-    sp = getservbyname("echo", "tcp");
-    /* get EchoServer Host information, NAME and INET ADDRESS */
+  int serverSocket_v, newSocket_v;
+  struct sockaddr_in serverAddr_v;
+  struct sockaddr_storage serverStorage_v;
 
-    gethostname(ThisHost, MAXHOSTNAME);
+  socklen_t addr_size_t, addr_size_v;
+  sem_init(&x, 0, 1);
+  sem_init(&y, 0, 1);
 
-    printf("----TCP/Server running at host NAME: %s\n", ThisHost);
-    if ((hp = gethostbyname(ThisHost)) == NULL)
-    {
-        fprintf(stderr, "Can't find host %s\n", ThisHost);
-        exit(-1);
+  serverSocket_t = socket(AF_INET, SOCK_STREAM, 0);
+  serverAddr_t.sin_addr.s_addr = INADDR_ANY;
+  serverAddr_t.sin_family = AF_INET;
+
+  serverSocket_v = socket(AF_INET, SOCK_STREAM, 0);
+  serverAddr_v.sin_addr.s_addr = INADDR_ANY;
+  serverAddr_v.sin_family = AF_INET;
+  if (argc == 2){
+    serverAddr_t.sin_port = htons(atoi(argv[1]));
+    serverAddr_v.sin_port = htons(atoi(argv[1])+1);
+  }else{
+    serverAddr_t.sin_port = htons(0);
+    serverAddr_v.sin_port = htons(0);
+  }
+
+  // Bind the socket to the
+  // address and port number.
+  bind(serverSocket_t, (struct sockaddr*)&serverAddr_t, sizeof(serverAddr_t));
+  bind(serverSocket_v, (struct sockaddr*)&serverAddr_v, sizeof(serverAddr_v));
+
+  // Listen on the socket,
+  // with 40 max connection
+  // requests queued
+  if (listen(serverSocket_t, 50) == 0)
+      printf("Listening Tellers\n");
+  else
+      printf("Error\n");
+
+  // Array for thread
+  pthread_t tid_t[60];
+
+  if (listen(serverSocket_v, 50) == 0)
+      printf("Listening Visualizers\n");
+  else
+      printf("Error\n");
+
+  // Array for thread
+  pthread_t tid_v[60];
+
+  FD_ZERO(&rset);
+
+  int i = 0;
+  int maxfdp1, tellerfd, visualizerfd, nready;
+
+  maxfdp1 = max(serverSocket_t, serverSocket_v) + 1;
+  for (;;) {
+
+    FD_SET(serverSocket_t, &rset);
+    FD_SET(serverSocket_v, &rset);
+
+    // select the ready descriptor
+    nready = select(maxfdp1, &rset, NULL, NULL, NULL);
+
+    if (FD_ISSET(serverSocket_t, &rset)) {
+      addr_size_t = sizeof(serverStorage_t);
+      // Extract the first
+      // connection in the queue
+      newSocket_t = accept(serverSocket_t,
+                          (struct sockaddr*)&serverStorage_t,
+                          &addr_size_t);
+      
+      if (pthread_create(&teller_threads[i++], NULL, recieveVote, &newSocket_t) != 0)
+        printf("Failed to create thread\n");
+    
     }
-    bcopy(hp->h_addr, &(server.sin_addr), hp->h_length);
-    printf("    (TCP/Server INET ADDRESS is: %s )\n", inet_ntoa(server.sin_addr));
+    
+    if (FD_ISSET(serverSocket_v, &rset)) {
+      addr_size_v = sizeof(serverStorage_v);
+      // Extract the first
+      // connection in the queue
+      newSocket_v = accept(serverSocket_v,
+                          (struct sockaddr*)&serverStorage_v,
+                          &addr_size_v);
+      
+      if (pthread_create(&visualizer_threads[i++], NULL, refreshData, &serverSocket_v) != 0)
+        printf("Failed to create thread\n");
 
-    /* Construct name of socket to send to. */
-    server.sin_family = hp->h_addrtype;
-    /*OR server.sin_family = AF_INET; */
-
-    server.sin_addr.s_addr = htonl(INADDR_ANY);
-
-    /*OR    server.sin_port = sp->s_port; */
-    if (argc == 2)
-        server.sin_port = htons(atoi(argv[1]));
-    else
-        server.sin_port = htons(0);
-
-    /*   Create socket on which to send  and receive */
-
-    /* sd = socket (PF_INET,SOCK_DGRAM,0); */
-    sd = socket(hp->h_addrtype, SOCK_STREAM, 0);
-
-    if (sd < 0)
-    {
-        perror("opening stream socket");
-        exit(-1);
     }
 
-    reusePort(sd);
 
-    if (bind(sd, (struct sockaddr *)&server, sizeof(server)))
-    {
-        close(sd);
-        perror("binding name to stream socket");
-        exit(-1);
-    }
 
-    length = sizeof(server);
-    if (getsockname(sd, (struct sockaddr *)&server, &length))
-    {
-        perror("getting socket name");
-        exit(0);
+    if (i >= 50) {
+      // Update i
+      i = 0;
+      while (i < 50) {
+        // Suspend execution of
+        // the calling thread
+        // until the target
+        // thread terminates
+        pthread_join(teller_threads[i++],
+                      NULL);
+        pthread_join(visualizer_threads[i++],
+        NULL);
+      }
+      // Update i
+      i = 0;
     }
+  }
 
-    printf("Server Port is: %d\n", ntohs(server.sin_port));
-    listen(sd, 4);
-    fromlen = sizeof(from);
-    for (;;)
-    {
-        psd = accept(sd, (struct sockaddr *)&from, &fromlen);
-        if (fork() == 0)
-        {
-            close(sd);
-            EchoServe(psd, from);
-        }
-    }
+  return 0;
+
 }
 
-void EchoServe(int psd, struct sockaddr_in from)
+int max(int x, int y)
 {
-    char buf[512];
-    int rc;
-    struct hostent *hp, *gethostbyname();
-
-    printf("Serving %s:%d\n", inet_ntoa(from.sin_addr),
-           ntohs(from.sin_port));
-    if ((hp = gethostbyaddr((char *)&from.sin_addr.s_addr,
-                            sizeof(from.sin_addr.s_addr), AF_INET)) == NULL)
-        fprintf(stderr, "Can't find host %s\n", inet_ntoa(from.sin_addr));
+    if (x > y)
+        return x;
     else
-        printf("(Name is : %s)\n", hp->h_name);
+        return y;
+}
 
+void recieveVote(void* param)
+{
+    int psd =  *((int *) param);
+    char buf[MAX_BUFF_SIZE];
+    int rc;
+   
     /*      get data from  clients and send it back */
     for (;;)
     {
@@ -137,13 +185,6 @@ void EchoServe(int psd, struct sockaddr_in from)
         {
             // Se imprime lo que se recibe : SECCION EN LA QUE SUMAMOS VOTOS
             buf[rc] = '\0';
-            /*
-            printf("Received: \"%s\"\n", buf);
-            printf("From TCP/Client: %s:%d\n", inet_ntoa(from.sin_addr),
-                   ntohs(from.sin_port));
-            printf("(Name is : %s)\n", hp->h_name);
-            */
-
             if (strncmp(buf, "a", 1) == 0)
             {
                 A = A+1;
@@ -155,30 +196,92 @@ void EchoServe(int psd, struct sockaddr_in from)
                 B = B+1;
                 printf("Se recibe voto para B, total : %i", B);
             }
-
-            // EN ESTA SECCION SE DEBEN ENVIAR LOS DOS NUEVOS VALORES DE A Y B, LOS CLIENTES QUE ESCUCHAN SE CONECTAN DE LA MISMO FORMA, LA UNICA DIF ES QUE EL RECUENTO SOLO ENVIA INFORMACION, Y LOS VISUALIZADORES ESCUCHAN SOLAMENTE
-            if (send(psd, buf, rc, 0) < 0)
-                perror("sending stream message");
         }
         else
         {
-            printf("TCP/Client: %s:%d\n", inet_ntoa(from.sin_addr),
-                   ntohs(from.sin_port));
-            printf("(Name is : %s)\n", hp->h_name);
-            printf("Disconnected..\n");
-            close(psd);
-            exit(0);
+           printf("Disconnected..\n");
+           pthread_exit(NULL);
         }
     }
 }
 
-void reusePort(int s)
-{
-    int one = 1;
 
-    if (setsockopt(s, SOL_SOCKET, SO_REUSEADDR, (char *)&one, sizeof(one)) == -1)
+void refreshData(void* param)
+{
+    int psd =  *((int *) param);
+    char buf[MAX_BUFF_SIZE];
+    int rc;
+    int initFlag = 0;
+    /*      get data from  clients and send it back */
+    for (;;)
     {
-        printf("error in setsockopt,SO_REUSEPORT \n");
-        exit(-1);
+      if (initFlag == 0)
+      {
+        printf("\nPoniendo al dia al Visualizador...\n"); 
+        sprintf(buf, "a,%d,b,%d\n", A, B);
+        printf(buf);
+        // send(psd, buf, sizeof(buf), 0);
+        initFlag++;
+      }else
+      {
+        if (A!= A_last || B!= B_last)
+        {
+          printf("\nActualizando info...\n"); 
+          sprintf(buf, "a,%d,b,%d\n", A, B);
+          printf(buf);
+          // send(psd, buf, sizeof(buf), 0);
+          A_last = A;
+          B_last = B;
+        }
+      }
+      // Encontrar condicional para llamar a estas funciones.
+      // printf("Disconnected..\n");
+      // pthread_exit(NULL);
+        
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
